d5744fe9f3237210b1f66063ad3a7cba7cc81e7b
dkrstic <ds612670@dal.ca>
2026-02-03 14:16:36 -0400

    Initial project code and logic, nested dictionary plus points and goals calculation

--- main.py
+++ main.py
@@ -1,1 +1,72 @@
-# Initial Dev Branch Test
+tournament = {
+    "player" : {
+        "marko" : {
+            "games_played",
+            "wins",
+            "draws",
+            "losses",
+            "goals_for",
+            "goals_against",
+            "goal_difference",
+            "points"
+        },
+        "dusan" : {
+            "games_played",
+            "wins",
+            "draws",
+            "losses",
+            "goals_for",
+            "goals_against",
+            "goal_difference",
+            "points"
+        },
+        "dimi": {
+            "games_played",
+            "wins",
+            "draws",
+            "losses",
+            "goals_for",
+            "goals_against",
+            "goal_difference",
+            "points"
+        }
+    }
+}
+
+while True:
+    home_name = input("Enter the home player name")
+    if home_name not in tournament["player"]:
+        print("Invalid input!")
+        continue
+    home_player = tournament["player"][home_name]
+
+    away_name = input("Enter the away player name")
+    if away_name not in tournament["player"]:
+        print("Invalid input!")
+        continue
+    away_player = tournament["player"][away_name]
+
+    home_goals = int(input("Enter home goals: "))
+    away_goals = int(input("Enter away goals: "))
+
+    tournament["player"][home_player]["goals_for"] += home_goals
+    tournament["player"][away_player]["goals_for"] += away_goals
+    tournament["player"][home_player]["goals_against"] += away_goals
+    tournament["player"][away_player]["goals_against"] += home_goals
+
+    if home_goals > away_goals:
+        tournament["player"][home_player]["points"] += 3
+        tournament["player"][home_player]["wins"] += 1
+        tournament["player"][away_player]["losses"] += 1
+    elif away_goals > home_goals:
+        tournament["player"][away_player]["points"] += 3
+        tournament["player"][away_player]["wins"] += 1
+        tournament["player"][home_player]["losses"] += 1
+    else:
+        tournament["player"][home_player]["points"] += 1
+        tournament["player"][home_player]["draws"] += 1
+        tournament["player"][away_player]["points"] += 1
+        tournament["player"][away_player]["draws"] += 1
+
+    
+    break
