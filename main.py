e83c4b9b6cca34da2b10bf0968b66f0b97abdf7b
dkrstic <ds612670@dal.ca>
2026-02-10 11:19:59 -0400

    Fixed dictionary, changed up loop logic

--- main.py
+++ main.py
@@ -1,72 +1,53 @@
 tournament = {
     "player" : {
         "marko" : {
-            "games_played",
-            "wins",
-            "draws",
-            "losses",
-            "goals_for",
-            "goals_against",
-            "goal_difference",
-            "points"
+            "games_played" : 0,
+            "wins" : 0,
+            "draws" : 0,
+            "losses" : 0,
+            "goals_for" : 0,
+            "goals_against" : 0,
+            "goal_difference" : 0,
+            "points" : 0
         },
         "dusan" : {
-            "games_played",
-            "wins",
-            "draws",
-            "losses",
-            "goals_for",
-            "goals_against",
-            "goal_difference",
-            "points"
+            "games_played" : 0,
+            "wins" : 0,
+            "draws" : 0,
+            "losses" : 0,
+            "goals_for" : 0,
+            "goals_against" : 0,
+            "goal_difference" : 0,
+            "points" : 0
         },
         "dimi": {
-            "games_played",
-            "wins",
-            "draws",
-            "losses",
-            "goals_for",
-            "goals_against",
-            "goal_difference",
-            "points"
+            "games_played" : 0,
+            "wins" : 0,
+            "draws" : 0,
+            "losses" : 0,
+            "goals_for" : 0,
+            "goals_against" : 0,
+            "goal_difference" : 0,
+            "points" : 0
         }
     }
 }
 
-while True:
-    home_name = input("Enter the home player name")
-    if home_name not in tournament["player"]:
-        print("Invalid input!")
-        continue
+num_of_games = int(input("Please enter the total amount of games: "))
+
+for x in range(num_of_games):
+    home_name = input("Please enter the home player's name: ")
+    away_name = input("Please enter the away player's name: ")
     home_player = tournament["player"][home_name]
-
-    away_name = input("Enter the away player name")
-    if away_name not in tournament["player"]:
-        print("Invalid input!")
-        continue
     away_player = tournament["player"][away_name]
+    home_player["games_played"] += 1
+    away_player["games_played"] += 1
 
-    home_goals = int(input("Enter home goals: "))
-    away_goals = int(input("Enter away goals: "))
-
-    tournament["player"][home_player]["goals_for"] += home_goals
-    tournament["player"][away_player]["goals_for"] += away_goals
-    tournament["player"][home_player]["goals_against"] += away_goals
-    tournament["player"][away_player]["goals_against"] += home_goals
+    print(f"{home_player} vs {away_player}")
+    home_goals = int(input("Enter home player's goals"))
+    away_goals = int(input("Enter away player's goals"))
 
     if home_goals > away_goals:
-        tournament["player"][home_player]["points"] += 3
-        tournament["player"][home_player]["wins"] += 1
-        tournament["player"][away_player]["losses"] += 1
-    elif away_goals > home_goals:
-        tournament["player"][away_player]["points"] += 3
-        tournament["player"][away_player]["wins"] += 1
-        tournament["player"][home_player]["losses"] += 1
-    else:
-        tournament["player"][home_player]["points"] += 1
-        tournament["player"][home_player]["draws"] += 1
-        tournament["player"][away_player]["points"] += 1
-        tournament["player"][away_player]["draws"] += 1
+        home_player["points"] += 3
 
     
-    break
